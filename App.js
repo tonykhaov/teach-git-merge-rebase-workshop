38417f6e607388375bc171fc075d1e6422f626b7
Tony Khaov <tony.khaov@gmail.com>
2021-07-26 14:57:38 +0200

    set up react-router-dom in App.js

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
 import * as React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function Home() {
-  return <h1>HOMEPAGE</h1>
+  return (
+    <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
+  )
 }
 
 export default Home
